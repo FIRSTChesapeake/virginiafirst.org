7e8c22bb1e960ae53b4250640ae295c2c99c4bba
Keith Walters <kwalters@taphere.com>
2013-08-05 12:52:48 -0400

    Added user model with devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Vafirst
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Speed up asset compilation by not loading the whole rails environment
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Vafirst::Application.routes.draw do
+  devise_for :users
+
+  get "home/index", as: "home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Vafirst::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
