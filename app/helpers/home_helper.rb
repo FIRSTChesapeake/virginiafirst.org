322c0bc7e670f4e1882e1ca048cebf793935d52e
Keith Walters <keith.walters@cattywamp.us>
2013-10-05 19:22:34 -0400

    Display next event on homepage

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+  def display_location(event)
+    buffer = ActiveSupport::SafeBuffer.new
+    buffer << "<address>".html_safe
+    buffer << "#{event.venue}<br>".html_safe if event.venue.present?
+    buffer << "#{event.city}, #{event.state}"
+    buffer << "</address>".html_safe
+    buffer
+  end
 end
