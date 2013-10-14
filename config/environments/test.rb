72c21b3fd8c453edc99c8bbb2d5889809068d169
Keith Walters <keith.walters@cattywamp.us>
2013-10-14 17:06:57 -0400

    Set test env default_url_host for mailer views

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,6 +29,9 @@ Vafirst::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Set the action mailer host name for urls
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
 
