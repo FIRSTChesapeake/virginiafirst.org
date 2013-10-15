a7318897b754eca8cf0298988d4a63d9647981dd
Keith Walters <keith.walters@cattywamp.us>
2013-10-15 12:40:43 -0400

    parameterize the application secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Vafirst::Application.config.secret_token = 'b507631d0d9f12a9cf0140cd84327babf195667fd1dd95b1fede1e05bfe787b7e21f033c6cd4b4af35a85c20c1f4afa5974b59176c1e0ca0cf22a4b810a00e69'
+Vafirst::Application.config.secret_token = ENV['SECRET_TOKEN']
