0d8cbb0839aded63d5be3631eadd96b8e136fc01
Keith Walters <keith.walters@cattywamp.us>
2014-03-11 22:27:30 -0400

    added ambassador table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140203215508) do
+ActiveRecord::Schema.define(:version => 20140304012413) do
+
+  create_table "ambassadors", :force => true do |t|
+    t.integer  "profile_id"
+    t.integer  "event_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "ambassadors", ["event_id"], :name => "index_ambassadors_on_event_id"
+  add_index "ambassadors", ["profile_id"], :name => "index_ambassadors_on_profile_id"
 
   create_table "assignments", :force => true do |t|
     t.integer  "user_id"
